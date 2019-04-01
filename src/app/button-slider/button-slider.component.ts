97d588afb97ba6aceb89a1cb45e104cd295009bb
Jacob Wright <jwright@hsfts.com>
2019-04-01 13:54:00 -0500

    wip

--- src/app/button-slider/button-slider.component.ts
+++ src/app/button-slider/button-slider.component.ts
@@ -7,9 +7,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonSliderComponent implements OnInit {
 
+  initialX: number = 0;
+  finalX: number = 0;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onPan(evt: any) {
+    var swipeMe = evt.target;
+    
+    var transformStyle = 'translateX(' + evt.center.x + 'px)';
+
+    swipeMe.style.transition = 'all 150ms ease-out';
+    swipeMe.style.webkitTransform = transformStyle;
+    swipeMe.style.transform = transformStyle;
+  }
+
+  onPanStart(evt: any) {
+    this.initialX = Number(evt.center.x);
+  }
+
+  onPanEnd(evt: any) {
+    this.finalX = Number(evt.center.x);
+    if(this.finalX - this.initialX > 0) {
+      this.rightSide(evt);
+    }
+    if(this.finalX - this.initialX < 0) {
+      this.leftSide(evt);
+    }
+  }
+
+  rightSide(evt: any) {
+    var swipeMe = evt.target;
+
+    let rightSide:number = document.getElementsByTagName("body").item(0).clientWidth * .9; 
+    
+    var transformStyle = 'translateX(' + rightSide + 'px)';
+
+    swipeMe.style.transition = 'all 150ms ease-out';
+    swipeMe.style.webkitTransform = transformStyle;
+    swipeMe.style.transform = transformStyle;
+  }
+
+  leftSide(evt: any) {
+    var swipeMe = evt.target;
+
+    let leftSide:number = document.getElementsByTagName("body").item(0).clientWidth * -.9; 
+    
+    var transformStyle = 'translateX(' + leftSide + 'px)';
+
+    swipeMe.style.transition = 'all 150ms ease-out';
+    swipeMe.style.webkitTransform = transformStyle;
+    swipeMe.style.transform = transformStyle;
+  }
+
+  onPanRight(evt: any) {
+    // var swipeMe = evt.target;
+    
+    // let deltaX: number  = Number(evt.deltaX);
+    // // console.log(deltaX);
+
+    // var transformStyle = 'translateX(' + deltaX + 'px)';
+
+    // swipeMe.style.transition = 'all 150ms ease-out';
+    // swipeMe.style.webkitTransform = transformStyle;
+    // swipeMe.style.transform = transformStyle;
+  }
+  
+  onPanLeft(evt: any) {
+    // var swipeMe = evt.target;
+    
+    // let deltaX: number  = Number(evt.deltaX);
+    
+    // var transformStyle = 'translateX(' + deltaX + 'px)';
+
+    // swipeMe.style.transition = 'all 150ms ease-out';
+    // swipeMe.style.webkitTransform = transformStyle;
+    // swipeMe.style.transform = transformStyle;
   }
 
 }
