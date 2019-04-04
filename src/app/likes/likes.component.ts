ab532fd617582b74a0ed224929770d242a9adde9
Jacob Wright <jwright@hsfts.com>
2019-04-04 12:49:48 -0500

    moved buttons to flyout

--- src/app/likes/likes.component.ts
+++ src/app/likes/likes.component.ts
@@ -10,27 +10,28 @@ export class LikesComponent implements OnInit {
   EPSILON: number = 50;
   initialX: number = 0;
   finalX: number = 0;
+  width: number = 0;
   currentX: number = 0;
-  clientWidth: number = 0;
 
-  sideAmount: number = .9;
+  sideAmount: number = .833;
 
-  target: any = null;
+  target: HTMLElement = null;
 
   constructor() { }
 
   ngOnInit() {
+    this.width = document.getElementsByTagName("body").item(0).clientWidth * this.sideAmount;
+    this.currentX = this.width;
   }
 
   onPanStart(evt: any) {
-    this.target = evt.target
-    this.clientWidth = evt.target.clientWidth - 10;
+    this.target = document.getElementById("top-pan-element");
     this.initialX = Number(evt.center.x);
   }
 
   onPan(evt: any) {
     let deltaX = evt.center.x - Math.abs(this.finalX - this.initialX);
-    this.translate(evt.target, this.currentX + deltaX);
+    this.translate(this.target, this.currentX + deltaX);
   }
 
   onPanEnd(evt: any) {
@@ -51,13 +52,13 @@ export class LikesComponent implements OnInit {
   }
 
   snapRight(evt: any) {
-    this.currentX = document.getElementsByTagName("body").item(0).clientWidth * this.sideAmount;
-    this.translate(evt.target, this.currentX);
+    this.translate(this.target, this.width);
+    this.currentX = this.width;
     this.resetX();
   }
 
   snapCenter(evt: any) {
-    this.translate(evt.target, 0);
+    this.translate(this.target, 0);
     this.currentX = 0;
     this.resetX();
   }
@@ -78,8 +79,8 @@ export class LikesComponent implements OnInit {
 
   sideViewChange() {
     if (this.sideAmount > 0 && this.target != null) {
-      this.currentX = document.getElementsByTagName("body").item(0).clientWidth * this.sideAmount;
-      this.translate(this.target, this.currentX);
+      this.width = document.getElementsByTagName("body").item(0).clientWidth * this.sideAmount;
+      this.translate(this.target, this.width);
     }
   }
 }
